44ec941288f24ca1af450bab33b93b654e02ff11
Timofei Irishiin <eblan2204.1@gmail.com>
2026-04-11 11:17:14 +0300

    Установлено фоновое изображение для титульного экрана игры

--- Connect4/TitleForm.Designer.cs
+++ Connect4/TitleForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TitleForm));
             lblTitle = new Label();
             btnStart = new Button();
             btnAbout = new Button();
@@ -35,26 +36,23 @@
             lblAuthor = new Label();
             pnlContent = new Panel();
             tblLayout = new TableLayoutPanel();
-
             pnlFooter.SuspendLayout();
-            tblLayout.SuspendLayout();
             pnlContent.SuspendLayout();
+            tblLayout.SuspendLayout();
             SuspendLayout();
-
             // 
             // lblTitle
             // 
             lblTitle.Anchor = AnchorStyles.None;
             lblTitle.AutoSize = true;
-            lblTitle.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
-            lblTitle.ForeColor = Color.FromArgb(45, 85, 165);
-            lblTitle.Location = new Point(100, 20);
+            lblTitle.Font = new Font("Segoe UI", 24F, FontStyle.Bold);
+            lblTitle.ForeColor = Color.SeaGreen;
+            lblTitle.Location = new Point(253, 62);
             lblTitle.Name = "lblTitle";
             lblTitle.Size = new Size(273, 54);
             lblTitle.TabIndex = 0;
             lblTitle.Text = "Connect Four";
             lblTitle.TextAlign = ContentAlignment.MiddleCenter;
-
             // 
             // btnStart
             // 
@@ -62,9 +60,9 @@
             btnStart.BackColor = Color.FromArgb(45, 125, 85);
             btnStart.FlatAppearance.BorderSize = 0;
             btnStart.FlatStyle = FlatStyle.Flat;
-            btnStart.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            btnStart.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
             btnStart.ForeColor = Color.White;
-            btnStart.Location = new Point(100, 100);
+            btnStart.Location = new Point(240, 181);
             btnStart.Name = "btnStart";
             btnStart.Size = new Size(300, 60);
             btnStart.TabIndex = 2;
@@ -73,7 +71,6 @@
             btnStart.Click += btnStart_Click;
             btnStart.MouseEnter += btnStart_MouseEnter;
             btnStart.MouseLeave += btnStart_MouseLeave;
-
             // 
             // btnAbout
             // 
@@ -81,9 +78,9 @@
             btnAbout.BackColor = Color.FromArgb(108, 117, 125);
             btnAbout.FlatAppearance.BorderSize = 0;
             btnAbout.FlatStyle = FlatStyle.Flat;
-            btnAbout.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            btnAbout.Font = new Font("Segoe UI", 14F);
             btnAbout.ForeColor = Color.White;
-            btnAbout.Location = new Point(100, 170);
+            btnAbout.Location = new Point(240, 247);
             btnAbout.Name = "btnAbout";
             btnAbout.Size = new Size(300, 50);
             btnAbout.TabIndex = 3;
@@ -92,7 +89,6 @@
             btnAbout.Click += btnAbout_Click;
             btnAbout.MouseEnter += btnAbout_MouseEnter;
             btnAbout.MouseLeave += btnAbout_MouseLeave;
-
             // 
             // pnlFooter
             // 
@@ -100,26 +96,26 @@
             pnlFooter.Controls.Add(lblAuthor);
             pnlFooter.Dock = DockStyle.Bottom;
             pnlFooter.Location = new Point(0, 400);
+            pnlFooter.MinimumSize = new Size(0, 50);
             pnlFooter.Name = "pnlFooter";
             pnlFooter.Size = new Size(800, 50);
             pnlFooter.TabIndex = 4;
-            pnlFooter.MinimumSize = new Size(0, 50); // Не сжимать футер
-
             // 
             // lblAuthor
             // 
             lblAuthor.AutoSize = true;
-            lblAuthor.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            lblAuthor.Font = new Font("Segoe UI", 9F);
             lblAuthor.ForeColor = Color.Gray;
             lblAuthor.Location = new Point(12, 18);
             lblAuthor.Name = "lblAuthor";
-            lblAuthor.Size = new Size(180, 20);
+            lblAuthor.Size = new Size(231, 20);
             lblAuthor.TabIndex = 0;
             lblAuthor.Text = "Разработчик: Иришин Тимофей";
-
             // 
             // pnlContent
             // 
+            pnlContent.BackgroundImage = (Image)resources.GetObject("pnlContent.BackgroundImage");
+            pnlContent.BackgroundImageLayout = ImageLayout.Stretch;
             pnlContent.Controls.Add(tblLayout);
             pnlContent.Dock = DockStyle.Fill;
             pnlContent.Location = new Point(0, 0);
@@ -127,48 +123,45 @@
             pnlContent.Padding = new Padding(10);
             pnlContent.Size = new Size(800, 400);
             pnlContent.TabIndex = 5;
-
             // 
-            // tblLayout - центрирующий контейнер
+            // tblLayout
             // 
+            tblLayout.BackColor = Color.Transparent;
             tblLayout.ColumnCount = 3;
-            tblLayout.RowCount = 4;
-            tblLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));  // Левый отступ (растягивается)
-            tblLayout.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));        // Центр (контент)
-            tblLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));  // Правый отступ (растягивается)
-            tblLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 0F));   // Пустой для выравнивания
-            tblLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 30F));        // Верхний отступ
-            tblLayout.RowStyles.Add(new RowStyle(SizeType.AutoSize));            // Заголовок
-            tblLayout.RowStyles.Add(new RowStyle(SizeType.AutoSize));            // Кнопка 1
-            tblLayout.RowStyles.Add(new RowStyle(SizeType.AutoSize));            // Кнопка 2
-            tblLayout.Dock = DockStyle.Top;
-            tblLayout.Location = new Point(10, 40); // Отступ сверху внутри pnlContent
-            tblLayout.Name = "tblLayout";
-            tblLayout.Size = new Size(780, 300);
-            tblLayout.TabIndex = 0;
-
-            // Размещение элементов в центральной колонке (индекс 1)
-            tblLayout.Controls.Add(lblTitle, 1, 1);
+            tblLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tblLayout.ColumnStyles.Add(new ColumnStyle());
+            tblLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tblLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 0F));
             tblLayout.Controls.Add(btnStart, 1, 2);
             tblLayout.Controls.Add(btnAbout, 1, 3);
-
+            tblLayout.Controls.Add(lblTitle, 1, 0);
+            tblLayout.Dock = DockStyle.Top;
+            tblLayout.Location = new Point(10, 10);
+            tblLayout.Name = "tblLayout";
+            tblLayout.RowCount = 4;
+            tblLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 30F));
+            tblLayout.RowStyles.Add(new RowStyle());
+            tblLayout.RowStyles.Add(new RowStyle());
+            tblLayout.RowStyles.Add(new RowStyle());
+            tblLayout.Size = new Size(780, 300);
+            tblLayout.TabIndex = 0;
             // 
-            // Connect4MainForm
+            // TitleForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
             Controls.Add(pnlContent);
             Controls.Add(pnlFooter);
-            MinimumSize = new Size(600, 450); // 🔒 Защита от слишком маленького окна
-            Name = "Connect4MainForm";
+            MinimumSize = new Size(600, 450);
+            Name = "TitleForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Connect Four";
-
             pnlFooter.ResumeLayout(false);
             pnlFooter.PerformLayout();
-            tblLayout.ResumeLayout(false);
             pnlContent.ResumeLayout(false);
+            tblLayout.ResumeLayout(false);
+            tblLayout.PerformLayout();
             ResumeLayout(false);
         }
 
